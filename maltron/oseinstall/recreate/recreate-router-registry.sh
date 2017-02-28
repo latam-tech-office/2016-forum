34c3983239e564feb9449676692f1132a311f178
root <root@master.example.com>
2017-02-28 07:44:04 -0500

    Removed old files

--- maltron/oseinstall/recreate/recreate-router-registry.sh
+++
@@ -1,4 +0,0 @@
-oc project default
-oadm registry --replicas=1 --service-account=registry --selector='host=infra' --config=/etc/origin/master/admin.kubeconfig --images='registry.access.redhat.com/openshift3/ose-${component}:${version}'
-
-oadm router default-router --stats-password='maltron' --selector='host=infra' --replicas=1 --service-account=router --config=/etc/origin/master/admin.kubeconfig --images='registry.access.redhat.com/openshift3/ose-${component}:${version}'
--- maltron/oseinstall/recreate/remove-enable-metrics.sh
+++
@@ -1,7 +0,0 @@
-oc delete all --selector="metrics-infra" -n openshift-infra
-oc delete sa --selector="metrics-infra" -n openshift-infra
-oc delete templates --selector="metrics-infra" -n openshift-infra
-oc delete secrets --selector="metrics-infra" -n openshift-infra
-oc delete pvc --selector="metrics-infra" -n openshift-infra
-oc delete sa metrics-deployer -n openshift-infra
-oc delete secret metrics-deployer -n openshift-infra
