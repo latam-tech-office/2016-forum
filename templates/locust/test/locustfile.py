e1e792fc0624f3afd674f52215180dbe37c05cc4
Pablo Szuster <pszuster@users.noreply.github.com>
2017-07-17 16:57:41 -0300

    hotelreq only

--- templates/locust/test/locustfile.py
+++ templates/locust/test/locustfile.py
@@ -4,7 +4,7 @@ class UserBehavior(TaskSet):
 
 	@task(1)
 	def book(self):
-    		self.client.post("/rest/book",  "{'flightReq': {'flightNo': 123,'flightDate': '01/01/2016', 'flightPassengers':2, 'flightFrom':'EZE','flightTo': 'GRU' },'hotelReq': {'hotelId': 123,'hotelArrivalDate': '01/01/2016','hotelNights': 10,'hotelCity': 'GRU' }, 'carReq': {'carRentalCo': 'Hertz','carStartDate': '01/01/2016','carType': 'abc','carDays': 3,'carCity': 'GRU'}}", headers={'Content-Type':'application/json'})
+    		self.client.post("/rest/book",  "{'hotelReq': {'hotelId': 123,'hotelArrivalDate': '01/01/2016','hotelNights': 10,'hotelCity': 'GRU' }}", headers={'Content-Type':'application/json'})
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
